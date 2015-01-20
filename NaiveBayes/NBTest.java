16a532481f40c7fb0007ff50961dbcc3c2c4b52e
Jiachen Li <jiacli.jk@gmail.com>
2015-01-20 00:42:46 -0500

    Added Naive Bayes

---
+++ NaiveBayes/NBTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author JK
+ *
+ */
+public class NBTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ NaiveBayes/NBTrain.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ */
+
+/**
+ * @author JK
+ *
+ */
+public class NBTrain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (line.length() == 0) {
+                continue;
+            }
+            
+            
+        }
+
+    }
+
+}
