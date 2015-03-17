1b8bf183faebd9c73fac9ea9d8bae86697289ecf
Jiachen Li <Jiacli@users.noreply.github.com>
2015-03-17 11:44:36 -0400

    Optimize LogReg

--- LogisticRegUsingSGD/LR.java
+++ LogisticRegUsingSGD/LR.java
@@ -10,10 +10,6 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 /**
- * 
- */
-
-/**
  * @author JK
  *
  */
@@ -22,10 +18,10 @@ public class LR {
 
     /**
      * @param args
-     * @throws IOException 
+     * @throws IOException
      */
     static int N;
-    
+
     public static void main(String[] args) throws IOException {
         if (args.length != 6) {
             System.err.println("Error: invalid arguments!");
@@ -33,19 +29,19 @@ public class LR {
         }
 
         // load parameters
-        N = Integer.parseInt(args[0]); // vocabulary size
+        N = Integer.parseInt(args[0]) / 10; // vocabulary size
         float lambda = Float.parseFloat(args[1]); // learning rate
         float mu = Float.parseFloat(args[2]); // regularization coefficient
         int T = Integer.parseInt(args[3]); // maximum iteration
         int sizeT = Integer.parseInt(args[4]); // size of trianing dataset
         String testset = args[5];
         HashMap<String, Integer> map = Initialize();
-        double[][] beta = new double[14][N];
-        int[][] A = new int[14][N]; 
-        
+        float[][] beta = new float[14][N];
+        short[][] A = new short[14][N];
+
         // training
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         String line = null;
         ArrayList<Integer> feats = new ArrayList<>();
         HashSet<String> labels = new HashSet<>();
@@ -54,13 +50,13 @@ public class LR {
         for (int t = 1; t < T; t++) {
             // update learning rate
             lambda_t = lambda / (t * t);
-            alpha = 1-2*lambda_t*mu;
-            
-            for (k = sizeT * (t-1); k < sizeT * t; k++) {
+            alpha = 1 - 2 * lambda_t * mu;
+
+            for (k = sizeT * (t - 1); k < sizeT * t; k++) {
                 line = br.readLine();
-//                if (line == null) {
-//                    continue;
-//                }
+                // if (line == null) {
+                // continue;
+                // }
                 tokenizeDoc(line, feats, labels);
                 for (Map.Entry<String, Integer> entry : map.entrySet()) {
                     if (labels.contains(entry.getKey())) {
@@ -69,33 +65,35 @@ public class LR {
                         y = 0.0;
                     }
                     int tag = entry.getValue();
+                    double z = 0.0;
+                    for (int id : feats) {
+                        z += beta[tag][id];
+                    }
+                    double val = lambda_t * (y - sigmoid(z));
                     // for each feature
                     for (int feat : feats) {
                         if (k - A[tag][feat] > 0) {
-                            beta[tag][feat] *= Math.pow(alpha, k-A[tag][feat]);
-                            A[tag][feat] = k;
+                            beta[tag][feat] *= Math
+                                    .pow(alpha, k - A[tag][feat]);
+                            A[tag][feat] = (short) k;
                         }
-                        double z = 0.0;
-                        for (int id : feats) {
-                            z += beta[tag][id];
-                        }
-                        double p = sigmoid(z);
-                        beta[tag][feat] += lambda_t * (y - p);
+                        beta[tag][feat] += val;
                     }
-                }
-            } 
-        }
-        br.close();
-        // reset every indicator to zero
-        for (int i = 0; i < beta.length; i++) {
-            for (int j = 0; j < beta[0].length; j++) {
-                if (k - A[i][j] > 0) {
-                    beta[i][j] *= Math.pow(alpha, k-A[i][j]);
                 }
             }
         }
-        
-        br = new BufferedReader(new InputStreamReader(new FileInputStream(testset)));
+        br.close();
+        // apply final regularization
+        for (int i = 0; i < beta.length; i++) {
+            for (int j = 0; j < beta[0].length; j++) {
+                if (k - A[i][j] > 0) {
+                    beta[i][j] *= Math.pow(alpha, k - A[i][j]);
+                }
+            }
+        }
+
+        br = new BufferedReader(new InputStreamReader(new FileInputStream(
+                testset)));
         double[] z = new double[14];
         while ((line = br.readLine()) != null) {
             tokenizeDocTest(line, feats);
@@ -106,56 +104,56 @@ public class LR {
             }
             StringBuilder sb = new StringBuilder();
             for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                double p = sigmoid(z[entry.getValue()]);
                 sb.append(entry.getKey());
                 sb.append("\t");
-                sb.append(p);
+                sb.append(sigmoid(z[entry.getValue()]));
                 sb.append(",");
             }
-            System.out.println(sb.substring(0, sb.length()-1));
+            System.out.println(sb.substring(0, sb.length() - 1));
             Arrays.fill(z, 0.0);
-        }        
+        }
         br.close();
-        
+
     }
-    
+
     private static double sigmoid(double z) {
         return 1.0 - 1.0 / (1.0 + Math.exp(z));
     }
-    
-    private static void tokenizeDoc(String cur_doc, ArrayList<Integer> feats, HashSet<String> labels) {
+
+    private static void tokenizeDoc(String cur_doc, ArrayList<Integer> feats,
+            HashSet<String> labels) {
         feats.clear();
         labels.clear();
-        
-        StringTokenizer st = new StringTokenizer(cur_doc, " \t\r\n_");
+
+        StringTokenizer st = new StringTokenizer(cur_doc, " \t\r\n");
         String label = st.nextToken(); // label will be stored at index 0
         for (String tag : label.split(",")) {
             labels.add(tag);
         }
-        
-        int sample = 0;
+
         while (st.hasMoreTokens()) {
             String word = st.nextToken();
-            if (word.length() > 3 && word.length() < 15 && sample++ % 2 == 0) {
-                int word_id = word.replaceAll("\\W", "").hashCode() % N;
-                while (word_id < 0) word_id += N;
+            if (word.length() > 3 && word.length() < 15) {
+                int word_id = word.hashCode() % N;
+                while (word_id < 0)
+                    word_id += N;
                 feats.add(word_id);
             }
         }
     }
-    
-    
+
     private static void tokenizeDocTest(String cur_doc, ArrayList<Integer> feats) {
         feats.clear();
-        
-        StringTokenizer st = new StringTokenizer(cur_doc, " \t\r\n_");
+
+        StringTokenizer st = new StringTokenizer(cur_doc, " \t\r\n");
         st.nextToken(); // label will be stored at index 0
-        
+
         while (st.hasMoreTokens()) {
             String word = st.nextToken();
             if (word.length() > 3 && word.length() < 15) {
-                int word_id = word.replaceAll("\\W", "").hashCode() % N;
-                while (word_id < 0) word_id += N;
+                int word_id = word.hashCode() % N;
+                while (word_id < 0)
+                    word_id += N;
                 feats.add(word_id);
             }
         }
