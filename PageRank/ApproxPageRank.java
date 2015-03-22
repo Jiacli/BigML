075df50df9becdad03d83537bd3457d129c960f5
Jiachen Li <Jiacli@users.noreply.github.com>
2015-03-22 18:22:06 -0400

    Added PageRank

---
+++ PageRank/ApproxPageRank.java
@@ -0,0 +1,26 @@
+
+
+/**
+ * Efficient Approximate PageRank
+ * @author jiacli
+ *
+ */
+public class ApproxPageRank {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        // load parameters
+        String inputPath = args[0];
+        String seed = args[1];
+        double alpha = Double.parseDouble(args[2]);
+        double epsilon = Double.parseDouble(args[3]);
+        
+        
+        
+        
+    }
+
+}
